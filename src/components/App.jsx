a01f1e58f21b8820f9081d8f78723b9b0a042e10
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-12-24 21:33:31 +0200

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,60 @@
-import React from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './Options/FeedbackOptions';
 import Notification from './Notification/Notification';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackSubmit = feedback => {
+    switch (feedback) {
+      case 'good':
+        feedBackGood();
+        break;
+      case 'neutral':
+        feedBackNeutral();
+        break;
+      case 'bad':
+        feedBackBad();
+        break;
+      default:
+        return;
+    }
   };
 
-  feedbackSubmit = data =>
-    this.setState(prevState => {
-      return { [data]: (prevState[data] += 1) };
-    });
+  const feedBackGood = () => setGood(good => good + 1);
+  const feedBackNeutral = () => setNeutral(neutral => neutral + 1);
+  const feedBackBad = () => setBad(bad => bad + 1);
 
-  countaTotal = () => Object.values(this.state).reduce((x, y) => x + y);
+  const countaTotal = () => good + neutral + bad;
+  const positivePercentage = () => Math.round((good * 100) / countaTotal());
 
-  positivePercentage = () =>
-    Math.round((this.state.good * 100) / this.countaTotal());
-
-  render() {
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.feedbackSubmit}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={feedbackSubmit}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countaTotal() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countaTotal()}
+            positive={positivePercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countaTotal() === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countaTotal()}
-              positive={this.positivePercentage()}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Options/FeedbackOptions.jsx
+++ src/components/Options/FeedbackOptions.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
